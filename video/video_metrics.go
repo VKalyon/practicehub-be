a5608d328d67085674272784759537c1dca6b99a
VKalyon <62647595+VKalyon@users.noreply.github.com>
2024-06-11 12:57:41 +0200

    metrics on request

--- video/video_metrics.go
+++ video/video_metrics.go
@@ -3,7 +3,6 @@ package video
 import (
 	"fmt"
 	"runtime"
-	"time"
 
 	"encore.dev/metrics"
 	"encore.dev/rlog"
@@ -12,15 +11,10 @@ import (
 var MemoryUsage = metrics.NewGauge[float64]("memory_usage_in_bytes", metrics.GaugeConfig{})
 
 func measureMemory() {
-	ticker := time.NewTicker(10 * time.Second)
-	defer ticker.Stop()
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
 
-	for range ticker.C {
-		var m runtime.MemStats
-		runtime.ReadMemStats(&m)
-
-		MemoryUsage.Set(float64(m.Alloc))
-		mem := fmt.Sprintf("Current memory usage is: %d bytes\n", m.Alloc)
-		rlog.Info(mem)
-	}
+	MemoryUsage.Set(float64(m.Alloc))
+	mem := fmt.Sprintf("Current memory usage is: %d bytes\n", m.Alloc)
+	rlog.Info(mem)
 }
